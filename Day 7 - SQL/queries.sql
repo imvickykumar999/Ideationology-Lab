bc14cd0085189a1ff87a42aa0c02a6391136c188
imvickykumar999 <imvickykumar999@gmail.com>
2021-12-14 23:21:43 +0530

    exporting csv from w3s

--- Day 7 - SQL/queries.sql
+++ Day 7 - SQL/queries.sql
@@ -1,3 +1,18 @@
 show databases;
 use w3schools;
 SELECT DISTINCT Country FROM Customers;
+SELECT Country FROM Customers;
+
+SELECT COUNT(DISTINCT Country) FROM Customers;
+
+SELECT * FROM Customers
+WHERE Country='Mexico';
+
+SELECT * FROM Customers
+WHERE CustomerID=1;
+
+SELECT * FROM Customers;
+SELECT * FROM Categories;
+SELECT * FROM Employees;
+SELECT * FROM Orders;
+SELECT * FROM Products;
