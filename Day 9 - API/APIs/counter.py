070dfc4f543a2cf5ccd96d5f6352031a24ab730f
imvickykumar999 <imvickykumar999@gmail.com>
2022-01-08 01:35:05 +0530

    Update counter.py

--- Day 9 - API/APIs/counter.py
+++ Day 9 - API/APIs/counter.py
@@ -1,8 +1,6 @@
 # !pip install firebase_admin
 
-import firebase_admin
 from firebase_admin import credentials
-from firebase_admin import db
 
 # Fetch the service account key JSON file contents
 cred = credentials.Certificate('vickservice-36ac9-firebase-adminsdk-ifzxv-e4df0f384c.json')
@@ -11,7 +9,11 @@ cred = credentials.Certificate('vickservice-36ac9-firebase-adminsdk-ifzxv-e4df0f
 url = 'https://vickservice-36ac9-default-rtdb.asia-southeast1.firebasedatabase.app/'
 path = {'databaseURL': url}
 
+import firebase_admin
 firebase_admin.initialize_app(cred, path)
 
+from firebase_admin import db
 refv = db.reference("counter/views")
 refv.set(refv.get()+1)
+
+print(refv.get())
