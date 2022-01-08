9c5996af82c768b303899ea2d2ca44e7bacab3b2
imvickykumar999 <imvickykumar999@gmail.com>
2022-01-08 16:00:10 +0530

    practice

---
+++ Day 9 - API/APIs/practice/counter.py
@@ -0,0 +1,20 @@
+
+from firebase_admin import credentials
+
+cred = credentials.Certificate('ideationology-4c639-firebase-adminsdk-5hfwu-5806b97f02.json')
+
+url = 'https://ideationology-4c639-default-rtdb.asia-southeast1.firebasedatabase.app/'
+path = {'databaseURL' : url}
+
+import firebase_admin
+firebase_admin.initialize_app(cred, path)
+
+from firebase_admin import db
+refv = db.reference('book/price')
+
+d = {'book1' : 190, 'book2' : 250}
+refv.set(d)
+
+refv = db.reference('book')
+g = refv.get()
+print(g)
