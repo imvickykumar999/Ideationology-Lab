484ad7e82225588c9a7d87046b6add709b1e61bb
imvickykumar999 <imvickykumar999@gmail.com>
2022-01-20 15:58:41 +0530

    news

--- Python Libraries/HTML/app.py
+++ Python Libraries/HTML/app.py
@@ -5,13 +5,39 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 
-    a=[]
-    n=3
-    for i in range(1, 11):
-        a.append(n*i)
-        
-    print(a)
-    return render_template('index.html', a=a)
+    import requests
+    import json
+
+    link = "https://api.imgflip.com/get_memes"
+    f = requests.get(link)
+    d = f.text
+    res = json.loads(d)
+    v = res['data']
+    mv = list(v.values())[0]
+
+    return render_template('index.html', mv=mv)
+
+
+@app.route('/news')
+def news():
+
+    from bs4 import BeautifulSoup
+    import requests
+
+    url = 'https://inshorts.com/en/read/'
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html5lib')
+
+    heading = soup.findAll('span', attrs = {'itemprop':'headline'})
+    imagex = soup.findAll('div', attrs = {'class':'news-card-image'})
+    bodyx = soup.findAll('div', attrs = {'itemprop':'articleBody'})
+    readmore = soup.findAll('div', attrs = {'class':'read-more'})
+
+    lst = [heading, imagex, bodyx, readmore]
+    return render_template('news.html',
+                            lst=lst
+                          )
+
 
 @app.errorhandler(404)
 def page_not_found(e):
