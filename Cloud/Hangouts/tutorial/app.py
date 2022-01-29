d2990486cc42851391302696844954df7b577a8e
imvickykumar999 <imvickykumar999@gmail.com>
2022-01-30 02:46:46 +0530

    send card and texts
    https://mail.google.com/chat/u/2/#chat/space/AAAA-pCtR2s

--- Cloud/Hangouts/tutorial/app.py
+++ Cloud/Hangouts/tutorial/app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def on_event():
   """Handles an event from Google Chat."""
-  event = request.get_json('ideationology-lab-339220-07656db2dff2.json')
+  event = request.get_json('ideationology-lab-339220-e996ca767b19.json')
   if event['type'] == 'ADDED_TO_SPACE' and not event['space']['singleUserBotDm']:
     text = 'Thanks for adding me to "%s"!' % (event['space']['displayName'] if event['space']['displayName'] else 'this chat')
   elif event['type'] == 'MESSAGE':
--- Cloud/Hangouts/tutorial/quickstart.py
+++ Cloud/Hangouts/tutorial/quickstart.py
@@ -7,14 +7,45 @@ from json import dumps
 from httplib2 import Http
 
 
-def main():
+def main(url):
     """Hangouts Chat incoming webhook quickstart."""
-    url = url2
+    url = url
     bot_message = {
-        'text' : '''Hey Vicks,
-        Hello from a Python script!
-        Its my first chat bot
-        '''}
+        'text' : text,
+          "cards": [
+            {
+              "sections": [
+                {
+                  "widgets": [
+                    {
+                      "textParagraph": {
+                        "text": "<b>Roses</b> are <font color=\"#ff0000\">red</font>,<br><i>Violets</i> are <font color=\"#0000ff\">blue</font>"
+                      }
+                    }
+                  ]
+                }
+              ]
+            },
+            {
+          "sections": [
+            {
+              "widgets": [
+                {
+                  "image": {
+                    "imageUrl": "https://developers.google.com/chat/images/cards-image.png",
+                    "onClick": {
+                      "openLink": {
+                        "url": "https://developers.google.com/chat/api/guides/message-formats/cards#image_widget/"
+                      }
+                    }
+                  }
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
 
     message_headers = {'Content-Type': 'application/json; charset=UTF-8'}
 
@@ -30,6 +61,26 @@ def main():
     print(response)
 
 if __name__ == '__main__':
+    text = input('Write message : ')
+
+    if text == '':
+        text = '''
+    Hey Vicks,
+    Hello from a Python script!
+    '''
+
     url1 = 'https://chat.googleapis.com/v1/spaces/AAAAOq_JWnk/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=0shxYWv8zlaNB9CVYeI5ayOKJhbyjqn562Cd5NmZXxU%3D'
     url2 = 'https://chat.googleapis.com/v1/spaces/AAAAe-ewe8U/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=08oGdB2rGkMAHPYXg3VmNWbLGG0jgLLAXwsl_F3Fsog%3D'
-    main()
+    url3 = 'https://chat.googleapis.com/v1/spaces/AAAAe-ewe8U/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=oHzjFlOMiHNYAVyceo9lZ21eoMRhSpIgV8fNM8AZB8M%3D'
+    url4 = 'https://chat.googleapis.com/v1/spaces/AAAA-pCtR2s/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=qnVg1YsSs0sTBr8Bs9jCBVfvHL0aPzBqjA49xpmq8oA%3D'
+    url5 = 'https://chat.googleapis.com/v1/spaces/AAAA-pCtR2s/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=8mgD7uBcYFeCiu-NnDqjjQNx1eVPGXfxbOzk_EoH72Q%3D'
+
+    lst = [url1, url2, url3, url4, url5]
+    url = input('Enter url_x number (1/2/3/4/5) : ')
+
+    if url == '':
+        url = lst[4]
+    else:
+        url = lst[int(url)-1]
+
+    main(url)
