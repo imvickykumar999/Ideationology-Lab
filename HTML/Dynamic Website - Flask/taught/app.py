b254f3bb3c6bfd3754e1b10816f6d4f74762dc2e
imvickykumar999 <imvickykumar999@gmail.com>
2022-02-17 15:41:19 +0530

    taught

--- HTML/Dynamic Website - Flask/taught/app.py
+++ HTML/Dynamic Website - Flask/taught/app.py
@@ -13,5 +13,9 @@ def index():
 def greet():
     return render_template('greet.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return ( render_template('404.html'), 404 )
+
 if __name__ == '__main__':
     app.run(debug=True)
