cc104d6835b95221de38c7080352341215845f7a
imvickykumar999 <imvickykumar999@gmail.com>
2022-05-20 02:12:48 +0530

    done

--- Hardwares/Experiments/bluetooth_LED/bluetooth_LED.ino
+++ Hardwares/Experiments/bluetooth_LED/bluetooth_LED.ino
@@ -1,4 +1,47 @@
 
+/*
+
+//https://forum.arduino.cc/t/strcmp-function-doesnt-work/568646
+
+char input[15];
+const int led = 13;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(led, OUTPUT);
+  Serial.println("Write 'switch on' or 'turn on' to HIGH");
+}
+
+void loop() {
+  int i=0;
+  
+  if (Serial.available()>0){
+    while (Serial.available()>0) {
+      
+      delay(2);  //delay to allow byte to arrive in input buffer
+      char c = Serial.read();
+      
+      input[i] = c;
+      i++;
+    }
+    Check (input);
+  }
+}
+
+void Check(char command[]){
+  int i=0;
+  
+  if ((strcmp(command, "switch on") == 10) || (strcmp(command, "turn on") == 10))
+    digitalWrite(led, HIGH);
+  
+  else
+    digitalWrite(led, LOW);
+}
+
+*/
+
+
+
 char inputByte;
 
 void setup() {
