01c023d7e340f1d036a0ebacfe4468396ac67df3
imvickykumar999 <imvickykumar999@gmail.com>
2022-05-26 00:49:44 +0530

    first

--- Python_To_APK/main.py
+++ Python_To_APK/main.py
@@ -1,51 +1,11 @@
 
 # pip install -r requirements.txt
 
-from kivymd.app import MDApp
-from kivy.lang import Builder
+from kivy.app import App
+from kivy.uix.button import Button
 
-class Test(MDApp):
+class TestApp(App):
     def build(self):
-        self.title = 'Programming'
-        self.theme_cls.primary_palette = "Blue"
-        return Builder.load_string(
-            '''
-BoxLayout:
-    orientation:'vertical'
-    MDToolbar:
-        title: 'Programming'
-        md_bg_color: app.theme_cls.primary_color
-        specific_text_color: 1, 1, 1, 1
+        return Button(text='My Name is Vicks.')
 
-    MDBottomNavigation:
-                
-        MDBottomNavigationItem:
-            name: 'screen 1'
-            text: 'Python'
-            icon: 'language-python'
-
-            Image:
-                id: imageView
-                source: 'python.png'
-        
-        MDBottomNavigationItem:
-            name: 'screen 2'
-            text: 'C++'
-            icon: 'language-cpp'
-
-            Image:
-                id: imageView
-                source: 'cp.png'
-
-        MDBottomNavigationItem:
-            name: 'screen 3'
-            text: 'Kivy'
-            icon: 'android'
-
-            Image:
-                id: imageView
-                source: 'kivy.png'
-'''
-        )
-
-Test().run()
+TestApp().run()
