f8d8afd558ea2cc1a44509985e72bda8379da371
imvickykumar999 <imvickykumar999@gmail.com>
2022-06-14 17:00:58 +0530

    mi remote

--- Hardwares/Extras/IR_Remote/IR_Remote.ino
+++ Hardwares/Extras/IR_Remote/IR_Remote.ino
@@ -2,10 +2,6 @@
 //https://www.tinkercad.com/things/jifiCImBU2C
 
 #include <IRremote.h>
-
-int red=9;
-int green=13;
-int blue=10;
 int RECV_PIN = 11;
 
 IRrecv irrecv(RECV_PIN);
@@ -13,10 +9,6 @@ decode_results results;
 
 void setup()
 {
-  pinMode(red,OUTPUT);
-  pinMode(blue,OUTPUT);
-  pinMode(green,OUTPUT);
-
   Serial.begin(9600);
   irrecv.enableIRIn(); // Start the receiver
 }
@@ -24,23 +16,7 @@ void setup()
 void loop() {
   if (irrecv.decode(&results)) {
     Serial.println(results.value, HEX);
-    irrecv.resume(); // Receive the next value
-    
-    if(results.value==0xFD08F7){
-      digitalWrite(red,HIGH);
-      digitalWrite(green,LOW);
-      digitalWrite(blue,LOW);
-    }
-       else if(results.value==0xFD48B7){
-      digitalWrite(red,LOW);
-      digitalWrite(green,LOW);
-      digitalWrite(blue,HIGH);
-    }
-       else if(results.value==0xFD8877){
-      digitalWrite(red,LOW);
-      digitalWrite(green,HIGH);
-      digitalWrite(blue,LOW);
-    }
+    irrecv.resume(); // Receive the next value    
   }
   delay(100);
 }
