b13fcb1b52268757e05fcec13c8225227a99c8e4
imvickykumar999 <imvickykumar999@gmail.com>
2023-11-20 11:29:48 +0530

    sd card manipulator

--- ADB GUI/ScrCpy_GUI.py
+++ ADB GUI/ScrCpy_GUI.py
@@ -1,11 +1,9 @@
 
-# https://github.com/imvickykumar999/ADB-Screen-Copy/releases
-# pyinstaller --onefile ScrCpy_GUI.py
-
-import os, json
-import threading
 from tkinter import *
-from tkinterweb import HtmlFrame
+import urllib.request
+import os, time, json, threading
+from tkinter import filedialog
+from ppadb.client import Client as AdbClient
 
 ip = input('''
 ----------------------------------------
@@ -17,6 +15,8 @@ Press ENTER for default IP
 
 Paste IP Address of Device : ''')
 
+try: os.mkdir('ScreenRecord')
+except: pass
 
 def task1():
     global ip
@@ -26,7 +26,7 @@ def task1():
     print("ID of process running task 1: {}".format(os.getpid()))
 
     if ip == '':
-        ip = '192.168.0.103'
+        ip = '192.168.0.102'
 
     while True:
         print()
@@ -48,13 +48,12 @@ def task2():
     def voldown():
         os.system(f'adb -s {ip} shell input keyevent 25')
 
-    def submit(x):
-        try: 
-            if x == '': 
-                os.system('Executable/keyevents.json')
-            else:
-                os.system(f'adb -s {ip} shell input keyevent {x}')
-        except Exception as e: 
+    def submit():
+        try:
+            urllib.request.urlretrieve("https://raw.githubusercontent.com/imvickykumar999/Android-Trojan-Horse/main/keyevents.json", "keyevents.json")
+            time.sleep(1)
+            root.destroy()
+        except Exception as e:
             print(e)
 
     def fun():
@@ -62,28 +61,79 @@ def task2():
         key = key.split(' : ')[0]
         os.system(f'adb -s {ip} shell input keyevent {key}')
 
+    def pull_file(file="screenshot.png", pull_start_path='Download/Telegram/static/'):        
+        client = AdbClient(host="127.0.0.1", port=5037)
+        device = client.device(f'{ip}:5555')
+
+        file = dir_list.get(dir_list.curselection()[0])
+        pull_start_path = entry.get()
+        event.set(f'{pull_start_path}/{file}')
+
+        if file[-1] == '/':
+            print(f'\n>>> "{pull_start_path}/{file}" Selected ...')
+
+        else:
+            try:
+                print(f'\n>>> Receiving {file} ...')
+                device.pull(f"/sdcard/{pull_start_path}/{file}", f"ScreenRecord/{file}")
+                print(f'\tReceived Successfully.')
+
+            except:
+                print(f'\tFile NOT Received.')
+            event.set(pull_start_path)
+
+    def push_file(push_stop_path='Download/Telegram/static/'):
+        client = AdbClient(host="127.0.0.1", port=5037)
+        device = client.device(f'{ip}:5555')
+
+        push_start_file = filedialog.askopenfilename()
+        push_stop_path = entry.get()
+
+        try:
+            file = os.path.basename(push_start_file)
+
+            if file.split('.')[-1] in ['apk', ]:
+                print(f'\n>>> Installing {file} ...')
+                device.install(push_start_file)
+                print(f'\n>>>\tInstalled.')
+
+            else:
+                print(f'\n>>> Sending {file} ...')
+                device.push(f"{push_start_file}", f"/sdcard/{push_stop_path}/{file}")
+                print(f'\tSent Successfully.')
+        except:
+            print(f'\tFile NOT Selected.')
+            pass
+ 
+    def back():
+        slash_path = entry.get()
+        slash = slash_path.split('/')
+        slash.pop()
+        slash.pop()
+        event.set('/'.join(slash) + '/')
+
     while True:
         root = Tk()
         root.geometry("300x600")
         root.title("ScrCpy GUI")
         root.config(bg="gray")
 
-        try:
-            bg = PhotoImage(file = "Executable/wallpaper.png") 
-            label = Label(root, image = bg) 
-            label.place(x = 0, y = 0) 
-        except:
-            frame = HtmlFrame(root)
-            frame.load_website("https://github.com/imvickykumar999/ADB-Screen-Copy/blob/main/Projects/TkinterGUI/Executable/wallpaper.png?raw=true")
-            frame.pack(fill="both", expand=True)
+        event = StringVar()
+        dir_list = Listbox(root, height=7, width=30)
+        dir_list.place(relx=0.5, rely=0.3, anchor='center')
 
-        try:
-            rely3 = 0.7
-            rely4 = 0.8
-            rely5 = 0.9
+        entry = Entry(root, textvariable = event)
+        entry.place(relx=0.56, rely=0.14, anchor='center', width=150)
 
-            num_list = Listbox(root, height=15, width=30)
-            with open('Executable/keyevents.json') as f:
+        back_btn = Button(root, bg='orange', text='. . /', command=back)
+        back_btn.place(relx=0.25, rely=0.14, anchor='center')
+
+        rely3 = rely4 = 0.8
+        rely5 = 0.9
+        try:
+
+            num_list = Listbox(root, height=8, width=30)
+            with open('keyevents.json') as f:
                 data = json.load(f)
 
             for i in data['key_events']:
@@ -91,31 +141,50 @@ def task2():
                 k = j.split('adb shell input keyevent ')
                 num_list.insert(k[1], f'{k[1]} : {i.split("key_")[1]}')
 
-            num_list.place(relx=0.5, rely=0.4, anchor='center')
-            get_num_btn = Button(root, bg='green', text="Run ADB", command=fun)
-            get_num_btn.place(relx=0.5, rely=0.1, anchor='center')
+            num_list.place(relx=0.5, rely=0.6, anchor='center')
+            get_num_btn = Button(root, bg='light green', text="Run ADB", command=fun)
+            get_num_btn.place(relx=0.5, rely=0.67, anchor='center')
 
         except:
-            rely3 = 0.5
-            rely4 = 0.6
-            rely5 = 0.8
+            btn = StringVar()
+            btn1 = Entry(root, textvariable = btn)
 
-            event = StringVar()
-            btn1 = Entry(root, textvariable = event)
+            btn1.insert(0, 'keyevent.json')
+            btn1.place(relx=0.5, rely=0.6, anchor='center')
 
-            btn1.insert(0, '209') # Open Music App
-            btn1.place(relx=0.5, rely=0.1, anchor='center')
+            btn2 = Button(root, bg='light green', text = 'Download', command=submit)
+            btn2.place(relx=0.5, rely=0.65, anchor='center')
 
-            btn2 = Button(root, bg='green', text = 'Keyevent', command=lambda: submit(event.get()))
-            btn2.place(relx=0.5, rely=0.2, anchor='center')
+        path='DCIM/Camera/'
+        event.set(path)
 
-        btn3 = Button(root, text="Volume Up", command=volup)
-        btn3.place(relx=0.5, rely=rely3, anchor='center')
+        upload = Button(root, bg='yellow', text='Send File', command = lambda: push_file(path))
+        upload.place(relx=0.5, rely=0.08, anchor='center')
 
-        btn4 = Button(root, text="Volume Down", command=voldown)
-        btn4.place(relx=0.5, rely=rely4, anchor='center')
+        def on_click():
+            path = event.get()
+
+            output = os.popen(f'adb shell ls -p /sdcard/{path}')
+            output = output.read().split('\n')[:-1]
+            dir_list.delete(0, END)
+
+            for i,j in enumerate(output):
+                dir_list.insert(i, j)
+            dir_list.place(relx=0.5, rely=0.3, anchor='center')
+
+        button = Button(root, bg='light blue', text="Update Folder", command=on_click)
+        button.place(relx=0.33, rely=0.43, anchor='center')
+
+        download = Button(root, bg='yellow', text='Receive Selected', command = lambda: pull_file(path))
+        download.place(relx=0.65, rely=0.43, anchor='center')
+
+        btn3 = Button(root, text="Volume Up", bg='green', command=volup)
+        btn3.place(relx=0.32, rely=rely3, anchor='center')
+
+        btn4 = Button(root, text="Volume Down", bg='red', command=voldown)
+        btn4.place(relx=0.63, rely=rely4, anchor='center')
         
-        btn5 = Button(root, bg='red', text="Power ON / OFF", command=power)
+        btn5 = Button(root, bg='pink', text="Power ON / OFF", command=power)
         btn5.place(relx=0.5, rely=rely5, anchor='center')
         root.mainloop()
 
